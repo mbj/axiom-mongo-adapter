ffeb1ee670b52a04b150292e05bbe7e12143a71e
Markus Schirp <mbj@seonic.net>
2012-08-01 14:11:01 +0200

    Do not run integration spec with remote interactions per default

--- spec/integration/read_spec.rb
+++ spec/integration/read_spec.rb
@@ -1,34 +1,36 @@
 require 'spec_helper'
 require 'logger'
 
-describe Adapter::Mongo, 'read' do
-  let(:uri)           { ENV.fetch('MONGO_URI', '127.0.0.1')                                    }
-  let(:logger)        { Logger.new($stdout)                                                    }
-          
-  let(:adapter)       { Adapter::Mongo.new(database)                                           }
-  let(:base_relation) { Relation::Base.new('people', [[:firstname,String],[:lastname,String]]) }
+if ENV['TRAVIS'] or ENV['HAS_MONGO']
+  describe Adapter::Mongo, 'read' do
+    let(:uri)           { ENV.fetch('MONGO_URI', '127.0.0.1')                                    }
+    let(:logger)        { Logger.new($stdout)                                                    }
+            
+    let(:adapter)       { Adapter::Mongo.new(database)                                           }
+    let(:base_relation) { Relation::Base.new('people', [[:firstname,String],[:lastname,String]]) }
 
-  let(:connection)    { Mongo::Connection.new(uri)                                             }
-                                                                                  
-  let(:relation)      { Adapter::Mongo::Gateway.new(adapter, base_relation)                    }
-  let(:database)      { connection.db('test')                                                  }
-  let(:collection)    { database.collection('people')                                          }
+    let(:connection)    { Mongo::Connection.new(uri)                                             }
+                                                                                    
+    let(:relation)      { Adapter::Mongo::Gateway.new(adapter, base_relation)                    }
+    let(:database)      { connection.db('test')                                                  }
+    let(:collection)    { database.collection('people')                                          }
 
-  before :all do
-    collection.insert(:firstname => 'John', :lastname => 'Doe')
-    collection.insert(:firstname => 'Sue', :lastname => 'Doe')
-  end
+    before :all do
+      collection.insert(:firstname => 'John', :lastname => 'Doe')
+      collection.insert(:firstname => 'Sue', :lastname => 'Doe')
+    end
 
-  specify 'it allows to receive all records' do
-    data = relation.to_a
-    data.should == [
-      [ 'John', 'Doe' ],
-      [ 'Sue', 'Doe' ]
-    ]
-  end
+    specify 'it allows to receive all records' do
+      data = relation.to_a
+      data.should == [
+        [ 'John', 'Doe' ],
+        [ 'Sue', 'Doe' ]
+      ]
+    end
 
-  specify 'it allows to receive specific records' do
-    data = relation.restrict { |r| r.firstname.eq('John') }.to_a
-    data.should == [ [ 'John', 'Doe' ] ]
+    specify 'it allows to receive specific records' do
+      data = relation.restrict { |r| r.firstname.eq('John') }.to_a
+      data.should == [ [ 'John', 'Doe' ] ]
+    end
   end
 end
