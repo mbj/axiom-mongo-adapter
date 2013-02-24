0a1b0c4e17b7dd9a4ee028f9d2c34fa0b7fedbcb
Markus Schirp <mbj@seonic.net>
2013-02-24 22:41:23 +0100

    Use spec infrastructure to rspec2

--- spec/shared/supported_unary_relation_method_behaviour.rb
+++ spec/shared/supported_unary_relation_method_behaviour.rb
@@ -2,9 +2,9 @@
 
 shared_examples_for 'a supported unary relation method' do
 
-  let(:adapter)   { mock('Adapter')                         }
-  let(:relation)  { mock('Relation', operation => response) }
-  let(:header)    { Relation::Header.new([[:id, Integer]])   }
+  let(:adapter)   { mock('Adapter')                           }
+  let(:relation)  { mock('Relation', operation => response)   }
+  let(:header)    { Relation::Header.coerce([[:id, Integer]]) }
   let(:response)  { mock('New Relation', :kind_of? => true, :class => factory, :header => header, :directions => header) }
   let!(:object)   { described_class.new(adapter, relation)  }
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,4 @@ Devtools.init_spec_helper
 include Veritas
 
 # A relation used in many specs
-BASE_RELATION = Relation::Base.new('name', [[:id,Integer],[:name,String]])
-
-Spec::Runner.configure do |config|
-  config.extend Spec::ExampleGroupMethods
-end
+BASE_RELATION = Relation::Base.coerce('name', [[:id,Integer],[:name,String]])
