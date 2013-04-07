a2a406fecdb35ff135a59593adb9cfa81c7c3cf0
Markus Schirp <mbj@schirp-dso.com>
2013-04-07 20:32:44 +0200

    Make sure with not get frozen

--- spec/shared/supported_unary_relation_method_behaviour.rb
+++ spec/shared/supported_unary_relation_method_behaviour.rb
@@ -5,7 +5,7 @@ shared_examples_for 'a supported unary relation method' do
   let(:adapter)   { mock('Adapter')                           }
   let(:relation)  { mock('Relation', operation => response)   }
   let(:header)    { Relation::Header.coerce([[:id, Integer]]) }
-  let(:response)  { mock('New Relation', :kind_of? => true, :class => factory, :header => header, :directions => header) }
+  let(:response)  { mock('New Relation', :kind_of? => true, :class => factory, :header => header, :directions => header, :frozen? => true) }
   let!(:object)   { described_class.new(adapter, relation)  }
 
   unless instance_methods.map(&:to_s).include?('block')
@@ -13,7 +13,7 @@ shared_examples_for 'a supported unary relation method' do
   end
 
   context 'first call' do
-    subject { object.public_send(operation, *arguments,&block) }
+    subject { object.public_send(operation, *arguments, &block) }
 
     before do
       described_class.stub!(:new).and_return(gateway)
